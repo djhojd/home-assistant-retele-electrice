738f542db51ae65a6f41053af89a2e8b3b0ff05b
Claudiu Hojda <claudiu.hojda@visma.com>
2026-04-30 01:35:55 +0300

    Add stat_id_prefix helper for statistic-id discovery

--- custom_components/retele_electrice/const.py
+++ custom_components/retele_electrice/const.py
@@ -6,3 +6,8 @@ CONF_POD = "pod"
 CONF_UPDATE_INTERVAL = "update_interval"
 
 DEFAULT_UPDATE_INTERVAL = 720  # default 12 hours in minutes
+
+
+def stat_id_prefix(pod: str) -> str:
+    """Return the statistic_id prefix used for all stats of a given POD."""
+    return f"{DOMAIN}:{pod.lower()}_"
